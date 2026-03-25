99beb9500929fb74d4073caf99cff9f9b760a914
ChlukasX <me@chlukas.xyz>
2026-03-25 21:43:15 +0100

    nvim changes with less and treesitter fix

--- config/nvim/lua/chlukas/plugins/treesitter.lua
+++ config/nvim/lua/chlukas/plugins/treesitter.lua
@@ -1,31 +1,34 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "master",
         build = ":TSUpdate",
         config = function()
-            require'nvim-treesitter.configs'.setup {
-              ensure_installed = { 
-                    "c", "go", "lua", "vim", 
-                    "vimdoc", "query", "markdown", 
-                    "markdown_inline", "typst", "java",
-                    "javascript", "typescript", "http",
-                    "python", "jsonc", "bash", 
-                    "nix",
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "go",
+                    "lua",
+                    "vim",
+                    "javascript",
+                    "typescript",
+                    "vimdoc",
                 },
-              auto_install = false,
-              indent = { enable = true },
-              highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-                additional_vim_regex_highlighting = false,
-              },
-            }
+                auto_install = true,
+                indent = { enable = true },
+                highlight = {
+                    enable = true,
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
         end,
-    }
+    },
 }
